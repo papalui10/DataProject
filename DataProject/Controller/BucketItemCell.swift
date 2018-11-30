9458380b1fbd3f7a75236f9f548592f33a67f362
Luis Veliz <luisvelizjr@gmail.com>
2018-11-30 13:28:43 -0700

    Added methods that check for errors and gets the bucketlist

--- DataProject/Controller/BucketItemCell.swift
+++ DataProject/Controller/BucketItemCell.swift
@@ -8,17 +8,65 @@
 
 import UIKit
 
-class BucketItemCell: UITableViewCell {
+class BucketItemCell: UITableViewCell
+{
 
-    override func awakeFromNib() {
+    override func awakeFromNib()
+    {
         super.awakeFromNib()
         // Initialization code
     }
 
-    override func setSelected(_ selected: Bool, animated: Bool) {
+    override func setSelected(_ selected: Bool, animated: Bool)
+    {
         super.setSelected(selected, animated: animated)
 
         // Configure the view for the selected state
     }
-
+    
+    @IBOutlet weak var bucketItemText: UILabel!
+    
+    @IBOutlet weak var bucketItemSymbol: UILabel!
+    
+    @IBOutlet weak var bucketItemSignature: UILabel!
+    
+    var currentBucketItem : BucketItem!
+    {
+        didSet
+        {
+            updateCellView()
+        }
+    }
+    
+    private func randomEmoji()->String
+    {
+        let emojiStart = 0x1F601
+        let emojiEnd = 0x1F64F
+        let symbolStart = 0x1F680
+        let symbolEnd = 0x1F6C5
+        
+        let emojiRange = 79
+        let symbolRange = 70
+        
+        let ascii = emojiStart + Int(arc4random_uniform(UInt32(emojiRange)))
+        let emoji = UnicodeScalar(ascii)?.description
+        return emoji!
+    }
+    
+    private func updateCellView() -> Void
+    {
+        if (currentBucketItem != nil)
+        {
+            bucketItemSignature.text = currentBucketItem.itemAuthor
+            bucketItemText.text = currentBucketItem.itemContents
+        }
+        else
+        {
+            bucketItemSignature.text = "Author goes here"
+            bucketItemText.text = "Bucket item contents here"
+        }
+        
+        bucketItemSymbol.text = randomEmoji()
+    }
+    
 }
--- DataProject/Controller/DataViewController.swift
+++ DataProject/Controller/DataViewController.swift
@@ -8,9 +8,11 @@
 
 import UIKit
 
-class DataViewController: UITableViewController {
+public class DataViewController: UITableViewController
+{
 
-    override func viewDidLoad() {
+  public override func viewDidLoad()
+    {
         super.viewDidLoad()
 
         // Uncomment the following line to preserve selection between presentations
@@ -22,25 +24,59 @@ class DataViewController: UITableViewController {
 
     // MARK: - Table view data source
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+   public override func numberOfSections(in tableView: UITableView) -> Int
+    {
+       
+        return 1
     }
 
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+   public override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        
+        return bucketList.count
     }
+    
+    lazy var bucketList : [BucketItem] =
+        {
+            return loadBucketListFromFile()
+    }()
+    
+    private func loadBucketListFromFile() -> [BucketItem]
+    {
+        var items = [BucketItem]()
+        do
+        {
+            if let filePath = Bundle.main.url(forResource: "bucket18", withExtension: "csv")
+            {
+                let input = try String(contentsOf: filePath)
+                let bucketLines = input.components(separatedBy: "\n")
+                for line in bucketLines
+                {
+                    let item = line.components(separatedBy: ",")
+                    items.append(BucketItem(contents: item[0], author: item[1]))
+                }
+            }
+        }
+    catch
+    {
+        print("File load errors")
+    }
+        
+    return items
+}
+    
+    
 
-    /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
+    
+  public override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+  {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath) as! BucketItemCell
+    
+        cell.currentBucketItem = bucketList[indexPath.row]
 
         return cell
     }
-    */
+
 
     /*
     // Override to support conditional editing of the table view.
