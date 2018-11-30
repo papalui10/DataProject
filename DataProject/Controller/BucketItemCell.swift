576f87e9b84ec8b5415f1182d0b69bd59f9be641
Luis Veliz <luisvelizjr@gmail.com>
2018-11-30 12:07:38 -0700

    Created BucketItemCell, dataViewController, also created model file and BucketItem.

---
+++ DataProject/Controller/BucketItemCell.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewCell.swift
+//  DataProject
+//
+//  Created by Veliz, Luis on 11/30/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+import UIKit
+
+class BucketItemCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
